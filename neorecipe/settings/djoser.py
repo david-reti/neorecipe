e522a4d1f7eb1a74a0fd6e99adfa7ff5d364bb7c
David Reti <development@davidreti.net>
2023-04-13 23:02:46 -0400

    Configure URL for password reset confirmation

--- neorecipe/settings/djoser.py
+++ neorecipe/settings/djoser.py
@@ -10,5 +10,6 @@ DJOSER = {
     "SET_USERNAME_RETYPE": True,
     "SET_PASSWORD_RETYPE": True,
     "SEND_ACTIVATION_EMAIL": True,
-    "ACTIVATION_URL": 'activate/{uid}/{token}',
+    "ACTIVATION_URL": "activate/{uid}/{token}",
+    "PASSWORD_RESET_CONFIRM_URL": "reset-password/{uid}/{token}"
 }
